9a74d20ccb9f05afe3ea8402cc487cb6dc782b45
Long Duc Hoang <duclongbox@gmail.com>
2023-07-07 19:00:31 -0400

    updated

--- workshop2/cstring.cpp
+++ workshop2/cstring.cpp
@@ -24,13 +24,6 @@ namespace sdds {
 
 	}
 	void strnCpy(char* des, const char* src, int len) {
-		/*int i;
-		for (i = 0; src[i] != '\0' && i<len; i++) {
-			des[i] = src[i];
-		}
-		if (i != len) {
-			des[i] == '\0';
-		}*/
 		int i = 0;
 
 		if (strLen(src) >= len) {
