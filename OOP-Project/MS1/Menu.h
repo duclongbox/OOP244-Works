8ce61875fdfcf38e1d6959b6ca83c7bcf5133a14
Long Duc Hoang <duclongbox@gmail.com>
2023-07-09 17:12:15 -0400

    updating

--- OOP-Project/MS1/Menu.h
+++ OOP-Project/MS1/Menu.h
@@ -1,14 +1,39 @@
 #ifndef MENU_H
 #define MENU_H
-class MenuItem {
-	char* m_content;
-	friend class Menu;
-	//MenuItem();
-	MenuItem(const char* content = nullptr);
-	MenuItem(const MenuItem& other) = delete;
-	MenuItem& operator=(const MenuItem& other) = delete;
-	~MenuItem();
-};
+#include <iostream>
 
+const unsigned  MAX_MENU_ITEMS = 20;
+namespace sdds {
+	class MenuItem {
+		char* m_content;
+		friend class Menu;
+		MenuItem() {};
+		MenuItem(const char* content = nullptr);
+		MenuItem(const MenuItem& other) = delete;
+		MenuItem& operator=(const MenuItem& other) = delete;
+		~MenuItem();
+		operator bool() const;
+		operator const char* () const;
+		void display(std::ostream& ostr ) const;
+		friend class Menu;
+	};
+	class Menu {
+		MenuItem m_title;
+		MenuItem* m_menuItems[MAX_MENU_ITEMS];
+		int m_numOfItems;
+
+	public:
+		Menu();
+		Menu(const char* title);
+		Menu(const Menu& other) = delete;
+		Menu& operator=(const Menu& other) = delete;
+		~Menu();
+		std::ostream& viewTitle(std::ostream& ostr) const;
+		std::ostream& viewMenu(std::ostream& ostr)const;
+		unsigned int run() const;
+		unsigned int operator~() const;
+
+	};
+}
 
 #endif
