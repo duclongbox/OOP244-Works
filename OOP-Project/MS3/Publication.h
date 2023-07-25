c764ca4e3beaa17b6a8cc21958d7d62e3a004747
Long Duc Hoang <duclongbox@gmail.com>
2023-07-25 09:31:02 -0400

    DONE

--- OOP-Project/MS3/Publication.h
+++ OOP-Project/MS3/Publication.h
@@ -1,3 +1,15 @@
+/*/////////////////////////////////////////////////////////////////////////
+						  Milestone-3
+Full Name  : Long Duc Hoang
+Student ID#: 122702228
+Email      : dhoang19@myseneca.ca
+Section    : ZAA
+Date	   : 7/25/2023
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
 #ifndef PUBLICATION_H
 #define PUBLICATION_H
 #include <iostream>
@@ -7,7 +19,7 @@
 using namespace std;
 namespace sdds {
 	class Publication: public Streamable {
-		char* m_title;
+		char* m_title{nullptr};
 		char m_shelfId[SDDS_SHELF_ID_LEN+1];
 		int m_membership;
 		int m_libRef;
--- OOP-Project/MS3/Streamable.cpp
+++ OOP-Project/MS3/Streamable.cpp
@@ -1,3 +1,15 @@
+/*/////////////////////////////////////////////////////////////////////////
+						  Milestone-3
+Full Name  : Long Duc Hoang
+Student ID#: 122702228
+Email      : dhoang19@myseneca.ca
+Section    : ZAA
+Date	   : 7/25/2023
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
 #include <iostream>
 #include "Streamable.h"
 
