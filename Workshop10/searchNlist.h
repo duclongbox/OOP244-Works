145acce28268eb70bee647fd46e2068a936fe579
Long Duc Hoang <duclongbox@gmail.com>
2023-08-06 10:37:28 -0400

    DONE

--- Workshop10/searchNlist.h
+++ Workshop10/searchNlist.h
@@ -1,3 +1,15 @@
+/*/////////////////////////////////////////////////////////////////////////
+						  Workshop - #10
+Full Name  : Long Duc Hoang
+Student ID#: 122702228
+Email      : dhoang19@myseneca.ca
+Section    : ZAA
+Date	   : 8/6/2023
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
 #ifndef SEARCHNLIST_LIST
 #define SEARCHNLIST_LIST
 #include "Collection.h"
@@ -6,6 +18,18 @@
 #include "Student.h"
 using namespace std;
 namespace sdds {
+	/// <summary>
+	/// 
+	/// T must support the "==" operator
+	/// T1 should not be a pointer,should be compatible with T so that the comparision can provide a valid result
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <typeparam name="T1"></typeparam>
+	/// <param name="collection">Templated object to store the searched value</param>
+	/// <param name="arrayObject"></param>
+	/// <param name="noOfElement"></param>
+	/// <param name="keyValue"> A value to search for </param>
+	/// <returns></returns>
 	template <typename T, typename T1>
 	bool search(Collection<T>& collection,   T* arrayObject, int noOfElement, T1 keyValue) {
 		// search function return true if one match is found
@@ -18,6 +42,13 @@ namespace sdds {
 		}
 		return result;
 	}
+	/// <summary>
+	/// T3 should provide the overload operator <<
+	/// </summary>
+	/// <typeparam name="T3"></typeparam>
+	/// <param name="title"></param>
+	/// <param name="objects"></param>
+	/// <param name="noOfElement"></param>
 	template <typename T3>
 	void listArrayElements(const char* title, const T3* objects, int noOfElement) {
 		// print the title
